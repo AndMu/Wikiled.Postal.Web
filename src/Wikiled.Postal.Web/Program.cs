24c137443d53f87c95ed5acabf5377c16d372a7b
Andrius Mudinas <keistokas@gmail.com>
2017-11-06 00:11:13 +0000

    Add logging

--- src/Wikiled.Postal.Web/Program.cs
+++ src/Wikiled.Postal.Web/Program.cs
@@ -1,18 +1,18 @@
-﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Wikiled.Postal.Web
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+      BuildWebHost(args).Run();
     }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()               
+            .Build();
+  }
 }
--- src/Wikiled.Postal.Web/Startup.cs
+++ src/Wikiled.Postal.Web/Startup.cs
@@ -1,43 +1,64 @@
-﻿using System.IO;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Web;
 
 namespace Wikiled.Postal.Web
 {
-    public class Startup
+  public class Startup
+  {
+    private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+    public Startup(IHostingEnvironment env)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.Use(async (context, next) => {
-                await next().ConfigureAwait(false);
-                if (context.Response.StatusCode == 404 &&
-                    !Path.HasExtension(context.Request.Path.Value) &&
-                    !context.Request.Path.Value.StartsWith("/api/"))
-                {
-                    context.Request.Path = "/index.html";
-                    await next().ConfigureAwait(false);
-                }
-            });
-
-            app.UseMvcWithDefaultRoute();
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-        }
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+        .AddEnvironmentVariables();
+      Configuration = builder.Build();
+      env.ConfigureNLog("nlog.config");
+      logger.Debug($"Starting: {Assembly.GetExecutingAssembly().GetName().Version}");
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      loggerFactory.AddNLog();
+
+      app.Use(async (context, next) =>
+      {
+        await next().ConfigureAwait(false);
+        if (context.Response.StatusCode == 404 &&
+            !Path.HasExtension(context.Request.Path.Value) &&
+            !context.Request.Path.Value.StartsWith("/api/"))
+        {
+          context.Request.Path = "/index.html";
+          await next().ConfigureAwait(false);
+        }
+      });
+
+      app.UseMvcWithDefaultRoute();
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+      app.AddNLogWeb();
+
+      env.ConfigureNLog("nlog.config");
+      LogManager.Configuration.Variables["logDirectory"] = Configuration.GetSection("logging").GetValue<string>("path");
+    }
+  }
 }
