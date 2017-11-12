f63ba16654b718985a7d894df4b970dd3d67d4ef
Andrius Mudinas <keistokas@gmail.com>
2017-11-12 20:39:24 +0000

    Setting up tests

--- src/Wikiled.Postal.Web/src/app/tabs/address.component.spec.ts
+++ src/Wikiled.Postal.Web/src/app/tabs/address.component.spec.ts
@@ -1,27 +1,34 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AddressComponent } from './address.component';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { PostalService } from '../service/postal.service';
+
 describe('AddressComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AddressComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AddressComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AddressComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AddressComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
+    let component: AddressComponent;
+    let fixture: ComponentFixture<AddressComponent>;
+    let service: PostalService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [DropDownsModule, ButtonsModule, HttpClientModule, HttpClientTestingModule],
+            declarations: [AddressComponent],
+            providers: [PostalService]
+        });
+
+        // create component and test fixture
+        fixture = TestBed.createComponent(AddressComponent);
+        // get test component from the fixture
+        component = fixture.componentInstance;
+        // UserService provided to the TestBed
+        service = TestBed.get(PostalService);
+    });
+
+    it('should create the app', async(() => {
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 });
