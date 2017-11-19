bbec78b7ff3fdccde29d575c302a73710a4c2117
Andrius Mudinas <keistokas@gmail.com>
2017-11-19 21:54:25 +0000

    Fixing tests

--- src/Wikiled.Postal.Web/src/app/controls/contact/contact.component.spec.ts
+++ src/Wikiled.Postal.Web/src/app/controls/contact/contact.component.spec.ts
@@ -2,24 +2,24 @@ import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { ContactComponent } from './contact.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { LocalService } from '../../service/local.service';
+import { PostalService } from '../../service/postal.service';
 
 describe('ContactComponent', () => {
 
     let component: ContactComponent;
     let fixture: ComponentFixture<ContactComponent>;
-    let service: LocalService;
+    let service: PostalService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientModule, HttpClientTestingModule ],
             declarations: [ContactComponent],
-            providers: [LocalService]
+            providers: [PostalService]
         });
 
         fixture = TestBed.createComponent(ContactComponent);
         component = fixture.componentInstance;
-        service = TestBed.get(LocalService);
+        service = TestBed.get(PostalService);
     });
 
     it('should create the app', async(() => {
