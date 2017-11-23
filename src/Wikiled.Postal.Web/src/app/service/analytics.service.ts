b9fb80b95670bc1c08dcb241220caf910fd3da22
Andrius <andrius@wikiled.com>
2017-11-23 13:15:30 +0000

    Changes to analytics

--- src/Wikiled.Postal.Web/src/app/service/analytics.service.ts
+++ src/Wikiled.Postal.Web/src/app/service/analytics.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class GoogleAnalyticsEventsService {
 
+    ga: Function;
+
     public emitEvent(eventCategory: string,
         eventAction: string,
         eventLabel: string = null,
         eventValue: number = null) {
-        ga('send', 'event', {
+        this.ga('send', 'event', {
             eventCategory: eventCategory,
             eventLabel: eventLabel,
             eventAction: eventAction,
